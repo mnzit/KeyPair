bfee3c4c3f310ac65a656acf1f2f4e8c0c5d2e13
manjit.shakya <manjit.shakya@f1soft.com>
2020-03-20 09:42:05 +0545

    add

---
+++ execute.bat
@@ -0,0 +1,3 @@
+openssl genrsa -out private_key.pem 2048
+openssl rsa -pubout -in private_key.pem -out public_key.pem
+openssl pkcs8 -topk8 -in private_key.pem -inform pem -out private_key_pkcs8.pem -outform pem -nocrypt
